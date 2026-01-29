acfd2997a80b44baabe0aed91dc70ff6001003bd
EHIENUIWAIUGDWLUOIHEAUOIHGE <plantsvsbrainrots986@gmail.com>
2026-01-29 15:46:24 -0600

    Create chestsData.lua

---
+++ chestsData.lua
@@ -0,0 +1,217 @@
+return {
+    chest = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    chestMediumIndustrial = {
+        ["chest"] = {
+            ["maxContentSlots"] = 12,
+            ["maxStackSize"] = 5,
+            ["automated"] = true,
+            ["total"] = 60
+        }
+    },
+    chestMedium = {
+        ["chest"] = {
+            ["maxContentSlots"] = 12,
+            ["maxStackSize"] = 60,
+            ["total"] = 720
+        }
+    },
+    chestMediumIndustrialIO = {
+        ["chest"] = {
+            ["maxContentSlots"] = 9,
+            ["maxStackSize"] = 30,
+            ["automated"] = true,
+            ["bidirectional"] = true,
+            ["total"] = 270
+        }
+    },
+    chestDesert = {
+        ["chest"] = {
+            ["maxContentSlots"] = 12,
+            ["maxStackSize"] = 60,
+            ["total"] = 720
+        }
+    },
+    treasureChestOld = {
+        ["chest"] = {
+            ["maxContentSlots"] = 9,
+            ["maxStackSize"] = 60,
+            ["total"] = 540
+        }
+    },
+    chestLargeIndustrialIO = {
+        ["chest"] = {
+            ["maxContentSlots"] = 12,
+            ["maxStackSize"] = 150,
+            ["automated"] = true,
+            ["bidirectional"] = true,
+            ["total"] = 1800
+        }
+    },
+    chestLarge = {
+        ["chest"] = {
+            ["maxContentSlots"] = 12,
+            ["maxStackSize"] = 150,
+            ["total"] = 1800
+        }
+    },
+    cauldronOld = {
+        ["chest"] = {
+            ["maxContentSlots"] = 9,
+            ["maxStackSize"] = 90,
+            ["total"] = 810
+        }
+    },
+    cauldron2Old = {
+        ["chest"] = {
+            ["maxContentSlots"] = 9,
+            ["maxStackSize"] = 90,
+            ["total"] = 810
+        }
+    },
+    diamondChestT1 = {
+        ["chest"] = {
+            ["maxContentSlots"] = 10,
+            ["maxStackSize"] = 1000,
+            ["total"] = 10000
+        }
+    },
+    diamondChestT2 = {
+        ["chest"] = {
+            ["maxContentSlots"] = 16,
+            ["maxStackSize"] = 1250,
+            ["total"] = 20000
+        }
+    },
+    securitySafe = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 1500,
+            ["total"] = 4500
+        }
+    },
+    lockerBlue = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    lockerRed = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    lockerGreen = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    lockerYellow = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    lockerPurple = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 30,
+            ["total"] = 180
+        }
+    },
+    schoolDeskRed = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 15,
+            ["total"] = 45
+        }
+    },
+    schoolDeskBlue = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 15,
+            ["total"] = 45
+        }
+    },
+    schoolDeskGreen = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 15,
+            ["total"] = 45
+        }
+    },
+    schoolDeskYellow = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 15,
+            ["total"] = 45
+        }
+    },
+    schoolDeskPurple = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 15,
+            ["total"] = 45
+        }
+    },
+    stockingSanta = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 5,
+            ["total"] = 15
+        }
+    },
+    stockingElf = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 5,
+            ["total"] = 15
+        }
+    },
+    stockingSnowman = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 5,
+            ["total"] = 15
+        }
+    },
+    stockingSnowflake = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 5,
+            ["total"] = 15
+        }
+    },
+    stockingCookie = {
+        ["chest"] = {
+            ["maxContentSlots"] = 3,
+            ["maxStackSize"] = 5,
+            ["total"] = 15
+        }
+    },
+    dungeonChestOld = {
+        ["chest"] = {
+            ["maxContentSlots"] = 6,
+            ["maxStackSize"] = 90,
+            ["total"] = 540
+        }
+    },
+    cauldron3Old = {
+        ["chest"] = {
+            ["maxContentSlots"] = 9,
+            ["maxStackSize"] = 90,
+            ["total"] = 810
+        }
+    }
+}
